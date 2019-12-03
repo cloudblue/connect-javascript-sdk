70d119523cda88398793aba6294d69f27f199bdd
Francesco Faraone <francesco.faraone@ingrammicro.com>
2019-12-03 11:57:40 +0100

    LITE-11011
    method approveWithTemplate implemented

--- test/connect/api/responses/requests/index.js
+++ test/connect/api/responses/requests/index.js
@@ -9,5 +9,7 @@ module.exports = {
     list_approved_pending_product: require('./list_approved_pending_product.json'),
     result_reject_request: require('./result_reject_request.json'),
     update_parameters: require('./update_parameters.json'),
+    update_parameters_without_note: require('./update_parameters_without_note.json'),
     update_parameters_error: require('./update_parameters_error.json'),
+    approve_request: require('./approve_request.json')
 }
