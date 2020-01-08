854d3942b7ccd70f66e2816226a58bc27b7e7c8c
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-01-08 16:03:44 +0100

    allow rql query as string

--- test/connect/api/utils.js
+++ test/connect/api/utils.js
@@ -12,7 +12,17 @@ const { rql2legacy } = require('../../../lib/connect/api/utils');
 
 
 describe('Connect Javascript SDK - Utils', () => {
-  it('rql2legacy throw an error if there is an unsupported operator', () => {
+  it('rql2legacy throw an error if there is an unsupported operator', (done) => {
     should(() => rql2legacy(new Query().or(new Query().eq('a', 0), new Query().eq('b', 1)))).throw(Error);
+    done();
+  });
+  it('rql2legacy accept string input', (done) => {
+    const params = rql2legacy('and(eq(a,0),eq(b,1),eq(test,ok),eq(bool,true))');
+    params.should.be.an.Object();
+    params.should.have.property('a').eql(0);
+    params.should.have.property('b').eql(1);
+    params.should.have.property('test').eql('ok');
+    params.should.have.property('bool').eql(true);
+    done();
   });
 });
