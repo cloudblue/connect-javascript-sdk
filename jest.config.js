ba17bfad4a6bd66704f735aa94f2a17bb4ecf32c
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-01-24 20:40:13 +0100

    fix travis coverage remove node 8

--- jest.config.js
+++ jest.config.js
@@ -35,6 +35,7 @@ module.exports = {
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: [
     "html",
+    "lcov",
   ],
 
   // An object that configures minimum threshold enforcement for coverage results
