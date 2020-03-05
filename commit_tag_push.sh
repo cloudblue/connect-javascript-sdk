729ab76f1e18eab6524d6997da3897386e61ad2e
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-03-05 14:54:11 +0100

    fix git script 2

--- commit_tag_push.sh
+++ commit_tag_push.sh
@@ -5,10 +5,12 @@ echo "Commit generated documentation"
 git config --global user.email "builds@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default simple
-git remote add origin https://${CONNECT_GITHUB_TOKEN}@github.com/cloudblue/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+echo "Configure remote"
+git remote add origin https://${CONNECT_GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
 export GIT_TAG=v$(node -p "require('./package.json').version")
 git add ./mddocs
-git commit -m "Documentation generated for release $GIT_TAG"
+echo "Commit docs"
+git commit -m "[ci skip] Documentation generated for release $GIT_TAG"
 echo "Push generated documentation and tag"
 git tag -a $GIT_TAG -m "Generated tag from TravisCI build"
 git push origin master && git push origin master --tags
