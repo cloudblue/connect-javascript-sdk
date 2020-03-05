063cea4e7fab172d1983b04ecfe19530efe985ff
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-03-05 15:48:27 +0100

    generate also jsdoc

--- commit_tag_push.sh
+++ commit_tag_push.sh
@@ -10,7 +10,7 @@ git remote set-url origin https://${CONNECT_GITHUB_TOKEN}@github.com/${TRAVIS_RE
 echo "Get version from package.json"
 export GIT_TAG=v$(node -p "require('./package.json').version")
 echo "Stage generated docs"
-git add ./mddocs
+git add .
 echo "Commit docs"
 git commit -m "[ci skip] Documentation generated for release $GIT_TAG"
 echo "Push generated documentation and tag"
