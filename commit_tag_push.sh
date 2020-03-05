3e7bbaa7aef8361a3c4e655d7cb304edec7b3ee8
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-03-05 15:03:05 +0100

    debug git script

--- commit_tag_push.sh
+++ commit_tag_push.sh
@@ -6,12 +6,15 @@ git config --global user.email "builds@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default simple
 echo "Configure remote"
-git remote add origin https://${CONNECT_GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+git remote add origin "https://${CONNECT_GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" > /dev/null 2>&1
+echo "Get version from package.json"
 export GIT_TAG=v$(node -p "require('./package.json').version")
+echo "Stage generated docs"
 git add ./mddocs
 echo "Commit docs"
 git commit -m "[ci skip] Documentation generated for release $GIT_TAG"
 echo "Push generated documentation and tag"
 git tag -a $GIT_TAG -m "Generated tag from TravisCI build"
+echo "Tag $GIT_TAG added"
 git push origin master && git push origin master --tags
 echo "Version $GIT_TAG successfully tagged!"
