e32092b68117b103fc249087c718f641767c1d0f
Martin Constante <martin.constante@ingrammicro.com>
2021-02-22 13:05:44 +0100

    fix ffaraone feedback

--- tests/specs/connect/api/conversations.spec.js
+++ tests/specs/connect/api/conversations.spec.js
@@ -34,14 +34,27 @@ describe('ConversationResource', () => {
 
   it('returns a list of messages of a conversation specified by its id', async () => {
     const conv = new ConversationResource(client);
-    const conversations = [
-      {
-        id: 'CV-001',
-        messages: []
-      }
-    ]
-    fetch.mockResponseOnce(JSON.stringify(conversations), { status: 200, headers: contentTypeJson });
-    await expect(conv.messages('PR-001').search()).resolves.toEqual(conversations);
+    const messages = {
+      id: "CO-000-000-000",
+      messages: [
+        {
+          id: 'ME-000-000-000',
+          conversation: 'CO-000-000-000',
+          created: '2018-12-18T13:03:30+00:00',
+          creator: {
+            id: 'UR-000-000-000',
+            name: 'Some User'
+          },
+          text: 'Hi, check out'
+        },
+      ],
+      creator: {
+        id: 'UR-000-000-000',
+        name: 'Some User'
+      },
+    };
+  fetch.mockResponseOnce(JSON.stringify(messages), { status: 200, headers: contentTypeJson });
+    await expect(conv.messages('PR-001').search()).resolves.toEqual(messages);
     expect(fetch).toBeCalledWith('https://localhost/conversations/PR-001/messages', expect.anything());
   });
 
